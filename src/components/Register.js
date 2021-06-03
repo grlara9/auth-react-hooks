c2af1aead270706beecbcf89f848a7deea9d3c9a
Guillermo Lara <grlara9@gmail.com>
2021-06-02 22:38:33 -0700

    started using useform and had to update to 7.0

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,6 @@
 import React from 'react'
  import {useForm} from 'react-hook-form'
+
 const Register=()=>{
     const {register, handleSubmit, error} = useForm();
 
@@ -10,12 +11,12 @@ const Register=()=>{
         <div className="register">
             <form onSubmit={handleSubmit(onSubmit)}> 
                 <label>Email</label> 
-                <input type="text" name="email" ref={register}/>
+                <input type="text" name="email" {...register('email')}/>
                 <label>Username</label>
-                <input type="text" name="username" ref={register}/>
-                <label>Password</label>
-                <input type="password" name="password" ref={register}/>
-                <button>Register</button>
+                <input type="text" name="username" {...register('username')}/>
+                <label>Password</label> 
+                <input type="password" name="password" {...register('password')}/>
+                <button type="submit">Register</button>
                 
             </form>
         </div>
