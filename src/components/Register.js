a1ebea7cd5f634cfd50f70a6f0cadbd34274b202
Guillermo Lara <grlara9@gmail.com>
2021-06-02 22:26:39 -0700

    importe the useform hook to handle validation on our form

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,16 +1,20 @@
 import React from 'react'
  import {useForm} from 'react-hook-form'
 const Register=()=>{
-    
+    const {register, handleSubmit, error} = useForm();
+
+    const onSubmit=(data)=>{
+        console.log(data)
+    }
     return (
         <div className="register">
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}> 
                 <label>Email</label> 
-                <input type="text" name="email" />
+                <input type="text" name="email" ref={register}/>
                 <label>Username</label>
-                <input type="text" name="username" />
+                <input type="text" name="username" ref={register}/>
                 <label>Password</label>
-                <input type="password" name="password" />
+                <input type="password" name="password" ref={register}/>
                 <button>Register</button>
                 
             </form>
