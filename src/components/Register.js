c5f93933ef9fa8693e8970f30212e56120bdcba4
Guillermo Lara <grlara9@gmail.com>
2021-06-03 21:44:47 -0700

    added first validation and display error if any

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import {useForm} from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 import './Register.css';
 const Register=()=>{
-    const {register, handleSubmit, error} = useForm();
 
+    const {register, handleSubmit, 
+        formState:{errors},} = useForm();
+    console.log("the errirs:" ,errors)
     const onSubmit=(data)=>{
         console.log(data)
     }
@@ -11,11 +13,20 @@ const Register=()=>{
         <div className="register">
             <form onSubmit={handleSubmit(onSubmit)}> 
                 <label>Email</label> 
-                <input type="text" name="email" {...register('email')}/>
+                <input
+                type="text" 
+                
+                {...register("email", { required: "Email is required"})}
+                />
+                {errors.email && (<p className="danger"> {errors.email.message}</p> )}
+
                 <label>Username</label>
                 <input type="text" name="username" {...register('username')}/>
                 <label>Password</label> 
-                <input type="password" name="password" {...register('password')}/>
+                <input type="password" name="password" {...register('password', {minLength: 6})
+            
+        }/>
+        
                 <button type="submit">Register</button>
                 
             </form>
