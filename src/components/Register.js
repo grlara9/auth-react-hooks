ef842f7ada97c4109c9befa9ab423942db1c0fd8
Guillermo Lara <grlara9@gmail.com>
2021-06-04 18:15:38 -0700

    added a regex pattern to username so it can only contain letter or numbers

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
 import './Register.css';
-const Register=()=>{
 
-    const {register, handleSubmit, 
-        formState:{errors, isDirty, isValid},} = useForm({mode: "onChange"});
-    console.log("the errirs:" ,errors)
+const Register=()=>{
+    const {register, handleSubmit, formState:{errors, isDirty, isValid},} = useForm({mode: "onChange"});
+    console.log("error object:" ,errors);
+
     const onSubmit=(data)=>{
         console.log(data)
     }
@@ -13,33 +13,45 @@ const Register=()=>{
         <div className="register">
             <form onSubmit={handleSubmit(onSubmit)}> 
                 <label>Email</label> 
-                <input
-                type="text" 
-                
-                {...register("email", { 
-                    required: "Email is required",
-                    pattern: {
-                        value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
-                        message: "Email format not valid"
-                    }
-                })}
-                />
-                {errors.email && (<p className="danger"> {errors.email.message}</p> )}
+                    <input
+                        type="text" 
+                        {...register("email", { 
+                            required: "Email is required",
+                            pattern: {
+                                value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+                                message: "Email format not valid"
+                                }})}/>
+                        {errors.email && (<p className="danger"> {errors.email.message}</p> )}
 
                 <label>Username</label>
-                <input type="text" name="username" {...register('username')}/>
+                    <input 
+                    type="text" 
+                    name="username" 
+                    {...register('username', {
+                        required: "Username is required",
+                        minLength: 6,
+                        maxLength: 12,
+                        pattern:{
+                            value: /^[A-Za-z0-9]+$/i,
+                            message: "Username contain uppercase , lowercase and number"
+                        }
+                    })}/>
+                
                 <label>Password</label> 
-                <input type="password" name="password" {...register('password',
-                {required: "Password is required",
-                minLength: 6,
-                pattern: {
-                    value: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/ , 
-                    message: "Password should contain at least one uppercase letter, lowercase letter, digit, and special symbol."
-                }
-            })}/>
-        {errors.password && (<p className="danger"> {errors.password.message}</p> )}
+                    <input 
+                        type="password" 
+                        name="password" 
+                        {...register('password',
+                            {required: "Password is required",
+                            minLength: 6,
+                            pattern: {
+                                value: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/ , 
+                                message: "Password should contain at least one uppercase letter, lowercase letter, digit, and special symbol."
+                            }
+                            })}/>
+                        {errors.password && (<p className="danger"> {errors.password.message}</p> )}
 
-                <button type="submit" disabled={!isDirty || !isValid}>Register</button>
+                        <button type="submit" disabled={!isDirty || !isValid}>Register</button>
                 
             </form>
         </div>
